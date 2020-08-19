81d544d0b19cd34a6c684aad56477194eaa176c8
Armenuhi <avaarm95@gmail.com>
2020-08-19 14:04:21 -0700

    updating repository with changes to app to create proper functionality

--- README.md
+++ README.md
@@ -1,58 +1,63 @@
-# 06-Weather-Dashbord
-
-(Weather Dashboard Using Server Side APIs)
-
-![weather dashboard demo](./Develop/06-server-side-apis-homework-demo.png)
+# [Weather-Dashboard]()
+[![GitHub License](https://img.shields.io/badge/License-orange.svg)](Develop/License/MIT.md)
+<!-- [![GitHub Test](https://img.shields.io/badge/Test-blue.svg)](#test) -->
 
 
+## Description 
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+Weather Dashboard
 
-
-
-## Documentation/ Tools Used
-
-Color and style determinations:
-Image Color picker in order to replicate exact requirements of desired interface - https://imagecolorpicker.com/en/
-
-API(s):
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
-
-Frameworks:
-Bootstrap (currently v4.4.1)
+This project has helped me gain a better understanding on the use of APIs: querying for particular parameters, using a .env & .gitignore to conceal private keys, as well as troubleshooting get requests.
 
 
-## User Story
+## Table of Contents
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+* [Technologies](#technologies)
+* [Demonstration](#demonstration)
+* [Application](#Application)
+* [Acknowledgements](#acknowledgements)
+* [Questions](#questions)
+* [Review](#review)
 
-## Acceptance Criteria
+## Technologies
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
+* JavaScript
+* Bootstrap
+* HTML
+* CSS
+* ajax 
+* axios 
+
+## Application 
+
+![Finished Product](/Develop/screenshot.png)
+
+## Demonstration 
+
+![Application Demo](/Develop/WeatherDashboard.gif)
+
+
+
+## Acknowledgements
+
+
+* [Common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates)
+* UW Coding Bootcamp instructional team and classmates
+    * test files already provided
+    * skeleton of html files already provided
+    * htmlRenderer.js already provided
+* [Bootstrap](https://getbootstrap.com/docs/4.1/getting-started/introduction/)
+
+
+## Questions 
+
+If you have any questions, please contact me on:
+GitHub at [avaarm](https://github.com/avaarm)
+Email at [Armenuhi](mailto:avaarm95@mail.com)
 
 
 
 ## Review
-
-You are required to submit the following for review:
 
 * The URL of the deployed application - https://github.com/avaarm/06-Weather-Dashbord
 
---
+++ index.js
@@ -0,0 +1,121 @@
+var locations = JSON.parse(localStorage.getItem("locationInput")) || [];
+
+
+function ajaxCall(forecastLocation) {
+  var query5D =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    forecastLocation +
+    "&appid=process.env.WeatherAPI_Key";
+    
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    forecastLocation +
+    "&appid=process.env.WeatherAPI_Key";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+
+  }).then(function (response) {
+    $("#currentData").empty();
+    var cityEl = $("<h2>").addClass("card-title");
+    var temperatureEl = $("<h5>");
+    var humidityEl = $("<h5>");
+    var windSpeedEl = $("<h5>");
+    var currentRow = $("<div>").attr("class", "row");
+
+    var day = date.getDate();
+    var date = new Date(response.dt * 1000);
+    var date = month + "/" + day + "/" + year;
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    
+    var humidityLevel = response.main.humidity;
+    var city = response.name;
+    var speedWind = response.wind.speed;
+    var fahrenheit = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+
+    cityEl.text(city + " " + date);
+    temperatureEl.text("Temperature: " + fahrenheit + " \xB0F");
+    humidityEl.text("Humidity: " + humidityLevel + " %");
+    windSpeedEl.text("Wind Speed: " + speedWind + " MPH");
+
+    var weatherIcon = response.weather[0].icon;
+    var iconSource = "https://openweathermap.org/img/w/" + weatherIcon + ".png";
+    var iconImg = $("<img>").attr({
+      alt: "Weather icon",
+      src: iconSource,
+      
+    });
+
+    $("#currentData").append(currentRow);
+    currentRow.append(cityEl, iconImg);
+    humidityEl.after(windSpeedEl);
+    temperatureEl.after(humidityEl);
+    currentRow.after(temperatureEl);
+   
+
+    $.ajax({
+      url: query5D,
+      method: "GET",
+    }).then(function (responseForecast) {
+      $("#forecast5Day").empty();
+      var title = $("<h3>").addClass("card-title");
+      title.text("5 Day Forecast");
+      $("#forecast5Day").prepend(title);
+      var dateRow = $("<div>").addClass("row date-row");
+      title.after(dateRow);
+
+      for (var i = 6; i <= 39; i = i + 8) {
+        var a = new Date(responseForecast.list[i].dt * 1000);
+        var dayForecast = a.getDate();
+        var monthForecast = a.getMonth() + 1;
+        var yearForecast = a.getFullYear();
+        var tempForecast = (
+          (responseForecast.list[i].main.temp - 273.15) * 1.8 +
+          32
+        ).toFixed(1);
+        var humidityForecast = responseForecast.list[i].main.humidity;
+        var weatherIconForecast = responseForecast.list[i].weather[0].icon;
+        var iconSourceForecast =
+          "http://openweathermap.org/img/w/" + weatherIconForecast + ".png";
+        var dateForecast =
+          monthForecast + "/" + dayForecast + "/" + yearForecast;
+        var cardDiv = $("<div>").addClass(
+          "card bg-primary date lg-col-2 med-col-4 sm-col-6"
+        );
+        var dateForecastEl = $("<h5>").addClass("card-title");
+        var iconImgForecast = $("<img>").attr({
+          src: iconSourceForecast,
+          alt: "Weather icon",
+        });
+        var tempForecastEl = $("<p>").addClass("text5day");
+        var humidityForecastEl = $("<p>").addClass("text5day");
+        cardDiv.append(
+          dateForecastEl,
+          iconImgForecast,
+          tempForecastEl,
+          humidityForecastEl
+        );
+        $(".date-row").append(cardDiv);
+        dateForecastEl.text(dateForecast);
+        tempForecastEl.text("Temp: " + tempForecast + " \xB0F");
+        humidityForecastEl.text("Humidity: " + humidityForecast + " %");
+      }
+    });
+  });
+}
+
+
+$(".fa-search").on("click", function (event) {
+  event.preventDefault();
+  $(".date-row").empty();
+  forecastLocation = $(".form-control").val();
+  if (locations.indexOf(forecastLocation) === -1) {
+    locations.push(forecastLocation);
+    localStorage.setItem("locationInput", JSON.stringify(locations));
+  }
+  renderlocations();
+  ajaxCall(forecastLocation);
+
+});
